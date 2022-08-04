38c0e850e65366f744750af5a3a44e88e52f2ace
Andrés González <andres.gonzalez@vizzuality.com>
2022-08-04 16:58:12 +0200

    tailwindcss@3.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ const aspectRatio = require('@tailwindcss/aspect-ratio');
 const forms = require('@tailwindcss/forms');
 const lineClamp = require('@tailwindcss/line-clamp');
 
+/**
+ * @type {import('tailwindcss').Config}
+ */
+
 module.exports = {
   content: [
     './src/components/**/*.@(tsx|ts)',
