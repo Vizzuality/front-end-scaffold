094eb7e9e525a9b7f47b346f5cfec2bdfda415b6
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2022-08-10 12:55:23 +0200

    Third party scripts: track page views

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
+import { useCallback, useEffect } from 'react';
+
 import { MapProvider } from 'react-map-gl';
 import { Provider as ReduxProvider } from 'react-redux';
 
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 
 import { OverlayProvider } from '@react-aria/overlays';
 import { QueryClient, QueryClientProvider, Hydrate } from '@tanstack/react-query';
@@ -10,30 +13,51 @@ import { SessionProvider } from 'next-auth/react';
 import ThirdParty from 'containers/third-party';
 
 import { MediaContextProvider } from 'components/media-query';
+import { GAPage } from 'lib/analytics/ga';
 import store from 'store';
 
 import 'styles/globals.css';
 
 const queryClient = new QueryClient();
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => (
-  <ReduxProvider store={store}>
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <SessionProvider session={pageProps.session} refetchInterval={10 * 60} refetchOnWindowFocus>
-          <OverlayProvider>
-            {/* @ts-ignore: https://github.com/artsy/fresnel/issues/281 */}
-            <MediaContextProvider>
-              <MapProvider>
-                <ThirdParty />
-                <Component {...pageProps} />
-              </MapProvider>
-            </MediaContextProvider>
-          </OverlayProvider>
-        </SessionProvider>
-      </Hydrate>
-    </QueryClientProvider>
-  </ReduxProvider>
-);
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  const handleRouteChangeCompleted = useCallback((url: string) => {
+    GAPage(url);
+  }, []);
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChangeCompleted);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChangeCompleted);
+    };
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <ReduxProvider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <SessionProvider
+            session={pageProps.session}
+            refetchInterval={10 * 60}
+            refetchOnWindowFocus
+          >
+            <OverlayProvider>
+              {/* @ts-ignore: https://github.com/artsy/fresnel/issues/281 */}
+              <MediaContextProvider>
+                <MapProvider>
+                  <ThirdParty />
+                  <Component {...pageProps} />
+                </MapProvider>
+              </MediaContextProvider>
+            </OverlayProvider>
+          </SessionProvider>
+        </Hydrate>
+      </QueryClientProvider>
+    </ReduxProvider>
+  );
+};
 
 export default MyApp;
