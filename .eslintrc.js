46a4bd5d26221192e590ebc0fc50c1ba679c41f7
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2023-01-17 19:03:05 +0100

    implements nextjs eslint rules/config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,7 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'next/core-web-vitals',
-    'airbnb-typescript',
-    'plugin:react/jsx-runtime',
-    'plugin:prettier/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-    tsconfigRootDir: __dirname,
-    project: './tsconfig.eslint.json',
-  },
-  plugins: ['@typescript-eslint'],
+  extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
   rules: {
     'no-console': [1, { allow: ['info', 'error'] }],
-    // NextJs specific fix: allow jsx syntax in js and ts files
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
     'react/jsx-props-no-spreading': [
       'error',
       {
@@ -34,16 +11,6 @@ module.exports = {
       },
     ],
     'import/no-named-as-default': 0,
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
     'import/order': [
       'warn',
       {
@@ -135,16 +102,5 @@ module.exports = {
         patterns: [{ group: ['lodash', '!lodash-es'], message: 'Use lodash-es instead' }],
       },
     ],
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-      typescript: {},
-    },
   },
 };
