72cb3328907e824d0e9ee4fba727d6ba6f4eeff4
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2023-06-16 09:48:01 +0200

    playwright: adds baseURL

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,9 +12,11 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './e2e',
   outputDir: './e2e/test-results',
+  /* Run your local dev server before starting the tests */
   webServer: {
     command: 'yarn dev',
     url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -29,7 +31,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -72,11 +74,4 @@ export default defineConfig({
     //   use: { ..devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
